8a8dc540b25be6130cdda7f270ab4b1bed7691e6
Pang0120 <lamkapang0120@gmail.com>
2023-04-21 14:47:54 +0800

    11

--- src/main/java/hkmu/comps380f/controller/IndexController.java
+++ src/main/java/hkmu/comps380f/controller/IndexController.java
@@ -1,6 +1,5 @@
 package hkmu.comps380f.controller;
 
-import hkmu.comps380f.dao.CommentService;
 import hkmu.comps380f.dao.TicketService;
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
     @Resource
-    private CommentService commentService;
-    @Resource
     private TicketService tService;
     @GetMapping("/")
     public String index() {
         return "redirect:/ticket/list";
-    }
-    @GetMapping("/test")
-    public String list(ModelMap model) {
-        model.addAttribute("CommentDatabase", commentService.getComments());
-        return "login";
     }
     @GetMapping("/login")
     public String login() {
